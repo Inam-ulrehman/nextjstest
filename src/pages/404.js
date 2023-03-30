3b342199e191cf5367ea7a20a2e3dc6582867d0d
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-30 09:23:19 -0400

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,10 @@
 import { imagesData } from '@/utils/data'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
-
+import { CldImage } from 'next-cloudinary'
 const Error = () => {
   const { headerHeight } = useSelector((state) => state.websitecontent)
   return (
@@ -15,7 +14,7 @@ const Error = () => {
         <meta name='description' content='Problem Generating page' />
       </Head>
       <Wrapper style={{ minHeight: `calc(100vh - ${headerHeight}px)` }}>
-        <Image
+        <CldImage
           src={imagesData.error}
           width={400}
           height={400}
