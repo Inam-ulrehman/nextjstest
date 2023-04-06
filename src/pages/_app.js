002b14698df19143dc16b4b54b02880d80c69c7e
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-06 09:28:28 -0400

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,12 @@ import store from '@/store'
 import Layout from '@/components/layout'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
+import { useSampleData } from '@/features/samples/swr'
 // import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
+  const sample = useSampleData({ page: 1, limit: 10 })
   return (
     <Provider store={store}>
       <Layout>
