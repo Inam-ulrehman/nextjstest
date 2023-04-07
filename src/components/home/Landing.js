30b6868e47f15ad545b25e179f1bc9b7b2e7a899
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-04-07 08:43:01 -0400

    f

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Landing = () => {
-  return <div>Landing</div>
+  return <Wrapper>Landing</Wrapper>
 }
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - 94px);
+`
 
 export default Landing
